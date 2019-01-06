87af2d6d801f716f591231a08955dcd2fb387395
Stewart ilondanga <stilondanga@gmail.com>
2019-01-06 21:09:59 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,14 @@ var navigate = (function() {
   });
 })();
 
-/*$(function(){
-		var btnSubmit = $('#submit');
-		btnSubmit.attr('disabled', 'disabled');
-		$('input[name=chosen]').change(function(e){
-			if($(this).val() == 'agree'){
-				btnSubmit.removeAttr('disabled');
-			}else{
-				btnSubmit.attr('disabled', 'disabled');
-			}
-		});
-	});
+$(function() {
+  var btnSubmit = $('#submit');
+  btnSubmit.attr('disabled', 'disabled');
+  $('input[name=chosen]').change(function(e) {
+    if ($(this).val() == 'agree') {
+      btnSubmit.removeAttr('disabled');
+    } else {
+      btnSubmit.attr('disabled', 'disabled');
+    }
+  });
+});
