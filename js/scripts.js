496b68cc842205532969e5f873990a170628880a
Stewart ilondanga <stilondanga@gmail.com>
2019-01-06 20:14:06 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,15 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+$(function(){
+		var btnSubmit = $('#submit');
+		btnSubmit.attr('disabled', 'disabled');
+		$('input[name=chosen]').change(function(e){
+			if($(this).val() == 'agree'){
+				btnSubmit.removeAttr('disabled');
+			}else{
+				btnSubmit.attr('disabled', 'disabled');
+			}
+		});
+	});
